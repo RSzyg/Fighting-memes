8da9d70ea112d10981376f2260cadb9aba8a0728
RSzyh <750332908@qq.com>
2018-01-31 23:24:07 +0800

    main.ts init

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,5 @@ window.onload = function() {
     let div: HTMLElement = document.getElementById('display');
     let para: HTMLElement = document.createElement('p');
     para.innerHTML = 'Hello!';
-    div.appendChild(para
+    div.appendChild(para);
 }
