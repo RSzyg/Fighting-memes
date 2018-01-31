81c7fe1180862ccbdc70aa4f83c92754bf416fdb
RSzyh <750332908@qq.com>
2018-01-31 23:22:11 +0800

    init project

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+window.onload = function() {
+    let div: HTMLElement = document.getElementById('display');
+    let para: HTMLElement = document.createElement('p');
+    para.innerHTML = 'Hello!';
+    div.appendChild(para
+}
