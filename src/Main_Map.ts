301895d8d3ede8e4e16f43cfc6fc0e48b9e4500f
huqiuhao <32033543+huqiuhao@users.noreply.github.com>
2018-02-05 19:36:42 +0800

    add determinant deciding if role knock into a wall
    1.update the new method to createScene, please replace the original createScene to see if the knock-wall determinant works.
    2.there are still some mistakes so that it cant move accurately and tightly close to a wall, so the alternative we use now is to ensure the selfRole can stop rightly near a wall

--- src/Main_Map.ts
+++ src/Main_Map.ts
@@ -8,7 +8,6 @@ class Main {
     private stageWidth: number;
     private stageHeight: number;
     private verticalSpacing: number;
-    private interval: number;
     private selfRole: Role;
     private blockThickness: number;
     private map: string[];
@@ -17,16 +16,27 @@ class Main {
         this.floors = [];
         this.stageWidth = 1920;
         this.stageHeight = 1080;
-        this.verticalSpacing = 120;
+        this.verticalSpacing = 0;
         this.blockThickness = 60;
-        this.interval = 17;
         this.map = [
-            "xxx      xx    xx    xx      xxx",
-            "      xxxxxxxxxxxxxxxxxxxx      ",
-            "  xxxxxx      xxxx      xxxxxx  ",
-            "xxxxxx     xxxxxxxxxx     xxxxxx",
-            "   xxxxxxxxxx      xxxxxxxxxx   ",
-            "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+            "                                ",
+            "                                ",
+            "XxY      XY    XY    XY      XxY",
+            "         XY          XY         ",
+            "         XY          XY         ",
+            "      XxxxxxxxxxxxxxxxxxxY      ",
+            "                                ",
+            "                                ",
+            "  XxxxxY                XxxxxY  ",
+            "              XxxY              ",
+            "            XxxxxxxY            ",
+            "XxxxY     XxxxxxxxxxxY     XxxxY",
+            "                                ",
+            "                                ",
+            "   XxxxxxxxxY      XxxxxxxxxY   ",
+            "                                ",
+            "                                ",
+            "XxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxY",
         ];
     }
     public createScene() {
@@ -35,15 +45,19 @@ class Main {
         this.stage.height = this.stageHeight;
         for (let i: number = 0; i < this.map.length; i++) {
             for (let j: number = 0; j < this.map[0].length; j++) {
-                    if (this.map[i].charAt(j) === "x") {
-                        const x: number = j * this.blockThickness;
-                        const y: number = i * (this.blockThickness + this.verticalSpacing) + this.verticalSpacing;
-                        const floor: Floor = new Floor(x, y, this.blockThickness, this.blockThickness, "basic");
-                        floor.setFillColor("#ffffff");
-                        floor.setStroke("#000000", 2);
-                        this.floors.push(floor);
-                        this.stage.add(floor.element);
-                    }
+                if (this.map[i].charAt(j) === "X") {
+                    const x: number = j * this.blockThickness;
+                    const y: number = i * (this.blockThickness + this.verticalSpacing) + this.verticalSpacing;
+                    const floorWidth = this.blockThickness * (this.map[i].indexOf("Y", j + 1) - j + 1);
+                    const floor: Floor = new Floor(x, y, floorWidth, this.blockThickness, "basic");
+                    floor.setFillColor("#ffffff");
+                    floor.setStroke("#000000", 2);
+                    this.floors.push(floor);
+                    this.stage.add(floor.element);
+                }
             }
         }
+        this.createRole(this.map.length * this.blockThickness - this.blockThickness);
+        document.addEventListener("keydown", (e) => this.keyboardController(e));
+        document.addEventListener("keyup", (e) => this.keyboardController(e));
     }
