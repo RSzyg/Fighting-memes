b6639f77f0f1b4c619a9df17e2bcf7904a80019e
RSzyh <750332908@qq.com>
2018-02-17 14:58:44 +0800

    remove redundant part of createRole

--- src/Main.ts
+++ src/Main.ts
@@ -63,16 +63,14 @@ class Main {
             for (let i = allRoles.length - 1; i >= 0; i--) {
                 if (i === allRoles.length - 1) {
                     this.selfId = allRoles[i].id;
-                    this.createRole(allRoles[i], "new");
-                } else {
-                    this.createRole(allRoles[i], "added");
                 }
+                this.createRole(allRoles[i]);
             }
         });
 
         this.socket.on("addRole", (data: string) => {
             const newRole = JSON.parse(data);
-            this.createRole(newRole, "new");
+            this.createRole(newRole);
         });
 
         this.socket.on("disconnect", () => {
@@ -170,28 +168,22 @@ class Main {
      * create a role
      * @param role role info
      */
-    private createRole(role: {[key: string]: any}, type: string) {
-        if (type === "added") {
-            this.Roles[role.id] = new Role(role.type, role.color, role.x, role.y);
-            this.stage.add(this.Roles[role.id].element);
-        }
-        if (type === "new") {
-            this.Roles[role.id] = new Role(role.type, role.color, role.x, role.y);
-            this.stage.add(this.Roles[role.id].element);
-            for (const floor of this.floors) {
-                if (
-                    role.y + this.Roles[role.id].height === floor.y &&
-                    role.x + this.Roles[role.id].width > floor.x &&
-                    role.x < floor.x + floor.width
-                ) {
-                    this.Roles[role.id].floor = floor;
-                    this.Roles[role.id].ladderY = floor.y;
-                }
+    private createRole(role: {[key: string]: any}) {
+        this.Roles[role.id] = new Role(role.type, role.color, role.x, role.y);
+        this.stage.add(this.Roles[role.id].element);
+        for (const floor of this.floors) {
+            if (
+                role.y + this.Roles[role.id].height === floor.y &&
+                role.x + this.Roles[role.id].width > floor.x &&
+                role.x < floor.x + floor.width
+            ) {
+                this.Roles[role.id].floor = floor;
+                this.Roles[role.id].ladderY = floor.y;
             }
-
-            // this.Roles[role.id].weapon = new Weapon(0, this.Roles[role.id].x, this.Roles[role.id].y);
-            // this.stage.addImage(this.Roles[role.id].weapon.image);
         }
+
+        // this.Roles[role.id].weapon = new Weapon(0, this.Roles[role.id].x, this.Roles[role.id].y);
+        // this.stage.addImage(this.Roles[role.id].weapon.image);
     }
     /**
      * handling the keyboard event
