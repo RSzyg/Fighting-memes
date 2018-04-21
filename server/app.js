269384dbf71f41274bb7236edfefa61099aa2c17
RSzyh <750332908@qq.com>
2018-04-21 11:42:55 +0800

    clear app.js

--- server/app.js
+++ server/app.js
@@ -90,164 +90,6 @@ io.on('connection', (socket) => {
         map: roomMap
     }
 
-    var impactJudge = function (nextX, nextWidth, isRight, id) {
-        if (Roles[id]) {
-            var i1 = Math.floor(Roles[id].y / initData.blockThickness);
-            var i2 = Math.floor((Roles[id].y + Roles[id].height) / initData.blockThickness);
-            var j1 = Math.floor(nextX / initData.blockThickness);
-            var j2 = Math.floor((nextX + nextWidth) / initData.blockThickness);
-
-            j1 = (j1 + roomMap[0].length) % roomMap[0].length;
-            j2 = (j2 + roomMap[0].length) % roomMap[0].length;
-
-            var j = isRight ? j2 : j1;
-
-            for (var i = i1; i <= i2; i++) {
-                if (roomMap[i] === undefined) {
-                    break;
-                }
-                if (
-                    (
-                        roomMap[i][j] !== "~" &&
-                        roomMap[i][j] !== " "
-                    )
-                ) {
-                    if (
-                        (Roles[id].y + Roles[id].height) !== i * initData.blockThickness &&
-                        j1 === j2 - 1
-                    ) {
-                        nextX = j2 * initData.blockThickness - isRight * nextWidth;
-                    }
-                }
-            }
-            Roles[id].x = (nextX + initData.stageWidth) % initData.stageWidth;
-
-            // console.log(Roles[id].x, Roles[id].y);
-        }
-    }
-
-    var fallJudge = function (id) {
-        if (Roles[id]) {
-            var x = Roles[id].x;
-            var i = Math.floor((Roles[id].y + Roles[id].height) / initData.blockThickness);
-            var j1 = Math.floor(x / initData.blockThickness);
-            x += Roles[id].width;
-            var j2 = Math.floor(x / initData.blockThickness);
-            if (roomMap[i] === undefined) {
-                return;
-            }
-            if (roomMap[i][j1] === " " && roomMap[i][j1] === roomMap[i][j2]) {
-                if (!Roles[id].verticalTimer) {
-                    console.log("fall true", Roles[id].x, Roles[id].y);
-                    Roles[id].i %= roomMap.length;
-                    Roles[id].jumpSpeed = 0;
-                    Roles[id].verticalTimer = setInterval(
-                        () => verticalMove(id),
-                        initData.interval
-                    );
-                }
-            }
-        }
-    }
-
-    var goDown = function (id) {
-        if (Roles[id]) {
-            var x = Roles[id].x;
-            // under foot block
-            var i = Roles[id].i + 1;
-            // left block
-            var j1 = Math.floor(x / initData.blockThickness);
-            // right block
-            x += Roles[id].width;
-            var j2 = Math.floor(x / initData.blockThickness);
-
-            if (
-                roomMap[i] === undefined ||
-                (
-                    roomMap[i][j1] === " " &&
-                    roomMap[i][j1] === roomMap[i][j2]
-                )
-            ) {
-                console.log("go down true", i);
-                Roles[id].i %= roomMap.length;
-                Roles[id].y += Roles[id].weight;
-                Roles[id].jumpSpeed = -2 * Roles[id].weight;
-                Roles[id].verticalTimer = setInterval(
-                    () => verticalMove(id),
-                    initData.interval,
-                );
-            }
-        }
-    }
-
-    var squatTreat = function (id, isDown) {
-        if (Roles[id]) {
-            var transferCoef = (2 * Number(isDown) - 1) * initData.transferCoef;
-            Roles[id].height -= transferCoef;
-            Roles[id].y += transferCoef;
-            console.log('squat', Roles[id].x, Roles[id].y);
-            var nextWidth = Roles[id].width + transferCoef;
-            impactJudge(nextWidth, 1, id);
-            Roles[id].width = nextWidth;
-        }
-    }
-
-    var verticalMove = function(id) {
-        var i;
-        var nextY = Roles[id].y - Roles[id].jumpSpeed;
-        var x = Roles[id].x;
-        Roles[id].jumpSpeed -= Roles[id].weight;
-        nextY = (nextY + initData.stageHeight) % initData.stageHeight;
-        if (Roles[id].jumpSpeed > 0) {
-            i = Math.floor(nextY / initData.blockThickness);
-            const j1 = Math.floor(x / initData.blockThickness);
-            x += Roles[id].width;
-            const j2 = Math.floor(x / initData.blockThickness);
-
-            if (roomMap[i]) {
-                if (
-                    (roomMap[i][j1] !== " " &&
-                        roomMap[i][j1] !== "~") ||
-                    (x !== j2 * initData.blockThickness &&
-                        roomMap[i][j2] !== undefined &&
-                        roomMap[i][j2] !== " " &&
-                        roomMap[i][j2] !== "~")
-                ) {
-                    Roles[id].jumpSpeed = 0;
-                    nextY = (i + 1) * initData.blockThickness;
-                }
-            }
-        } else if (Roles[id].jumpSpeed <= 0) {
-            i = Math.floor((nextY + Roles[id].height) / initData.blockThickness);
-            var j1 = Math.floor(x / initData.blockThickness);
-            x += Roles[id].width;
-            var j2 = Math.floor(x / initData.blockThickness);
-
-            j1 = (j1 + roomMap[0].length) % roomMap[0].length;
-            j2 = (j2 + roomMap[0].length) % roomMap[0].length;
-
-            if (
-                roomMap[i] &&
-                Roles[id].y + Roles[id].height <= i * initData.blockThickness
-            ) {
-                if (
-                    roomMap[i][j1] !== " " ||
-                    roomMap[i][j2] !== " " &&
-                    x !== j2 * initData.blockThickness
-                ) {
-                    nextY = i * initData.blockThickness - Roles[id].height;
-                    clearInterval(Roles[id].verticalTimer);
-                    Roles[id].verticalTimer = undefined;
-                }
-            }
-        }
-        Roles[id].y = nextY;
-
-        // update role's block pos
-        i = Math.floor((Roles[id].y + Roles[id].height) / initData.blockThickness);
-        Roles[id].i = i;
-    }
-
     const t1 = new Date().getTime();
     socket.emit('init', JSON.stringify(initData));
 
@@ -300,57 +142,23 @@ io.on('connection', (socket) => {
     socket.on('to jump', (data) => {
         socket.broadcast.emit('jump', data);
         var info = JSON.parse(data);
-        //jumpPreTreat
-        if (Roles[info.id] && !Roles[info.id].verticalTimer) {
-            Roles[info.id].jumpSpeed = Roles[info.id].power;
-            Roles[info.id].verticalTimer = setInterval(
-                () => verticalMove(info.id),
-                initData.interval,
-            );
-        }
     });
 
     socket.on('to squat', (data) => {
         socket.broadcast.emit('squat', data);
-        squatTreat(JSON.parse(data).id, true);
     });
 
     socket.on('to stand', (data) => {
         socket.broadcast.emit('stand', data);
-        squatTreat(JSON.parse(data).id, false);
     });
 
     socket.on('to go down', (data) => {
         socket.broadcast.emit('go down', data);
-        goDown(JSON.parse(data).id);
     });
 
     socket.on('to move', (data) => {
         socket.broadcast.emit('move', data);
         var info = JSON.parse(data);
-        try {
-            if (Roles[info.id] === undefined) {
-                throw "no such id";
-            }
-            var role = Roles[info.id];
-            var nextX = role.x;
-            if (info.isRight) {
-                nextX += role.moveSpeed;
-            } else {
-                nextX -= role.moveSpeed;
-            }
-            
-            //impactJudge
-            impactJudge(nextX, role.width, info.isRight, role.id);
-
-            var j = Math.floor(role.x / initData.blockThickness);
-            role.j = j;
-
-            fallJudge(role.id);
-        } catch (e) {
-            console.log(e);
-            socket.disconnect(true);
-        }
     });
 
     socket.on('disconnect', () => {
